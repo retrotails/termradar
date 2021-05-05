fbc20a5879ac491af062363c830628bfd249245d
retrotails <10767978+retrotails@users.noreply.github.com>
2021-05-05 17:19:21 -0400

    rearrange code use sys.exit instead of exit() (I'm not actually certain it's replacing exit(), both seem to work the same)

--- termradar.py
+++ termradar.py
@@ -16,6 +16,7 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+from sys import exit
 from os import get_terminal_size, path, makedirs, remove
 from os.path import expanduser
 from re import split, search
@@ -28,10 +29,167 @@ from subprocess import run, PIPE
 from glob import glob
 from configparser import ConfigParser
 config = ConfigParser()
-def clamp(v,n,x): return min(max(v,n),x)
 
-# Map resolution
-res_img = [7000, 3500]
+def clamp(v,n,x): return min(max(v,n),x)
+# https://stackoverflow.com/questions/54242194/python-find-the-closest-color-to-a-color-from-giving-list-of-colors/54244301#54244301
+def closest(color):
+	# clamp out the mostly useless "colder" colors that are just noise
+	if color[0]*2 + color[1]*1 - color[2]*3 < 128: return 0
+	color = np.array(color)
+	distances = np.sqrt(np.sum((col_actual-color)**2,axis=1))
+	return np.where(distances==np.amin(distances))[0][0]
+def is_int(i):
+	try: int(i); return True
+	except ValueError: return False
+
+# Terminal control characters for the various colors
+col_fg = [
+	"\033[1;30m","\033[1;31m","\033[1;32m","\033[1;33m",
+	"\033[1;34m","\033[1;35m","\033[1;36m","\033[1;37m",
+	"\033[1;90m","\033[1;91m","\033[1;92m","\033[1;93m",
+	"\033[1;94m","\033[1;95m","\033[1;96m","\033[1;97m",
+]
+col_bg = [
+	"\033[0;40m","\033[0;41m","\033[0;42m","\033[0;43m",
+	"\033[0;44m","\033[0;45m","\033[0;46m","\033[0;47m",
+	"\033[0;100m","\033[0;101m","\033[0;102m","\033[0;103m",
+	"\033[0;104m","\033[0;105m","\033[0;106m","\033[0;107m",
+]
+res_img = [7000, 3500] # Map resolution
+
+# Scrape TIF images from NOAA using some lazy commands
+def get_map(frames, location):
+	base_url = "https://mrms.ncep.noaa.gov/data/RIDGEII/L2/CONUS/CREF_RAW/"
+	def get_tif(url):
+		tif = run(["wget", "-O", "-", "-o", "/dev/null", base_url + url + ".tif.gz"], check=True, capture_output=True)
+		out = run(["gunzip", "-c"], input=tif.stdout, capture_output=True)
+		return out.stdout
+	for f in glob(location + "/" + "*.tif"):
+		remove(f)
+	# Get latest list of images
+	result = run(["wget", "-O", "-", "-o", "/dev/null", base_url], stdout=PIPE)
+	files = []
+	for line in str(result).split('\\n'):
+		if ".tif.gz" in line:
+			files.append(search('<a href="(.*).tif.gz">', line).group(1))
+	# Download and save to file
+	i = 0
+	for f in range(frames):
+		print("Downloading frame " + str(f))
+		fil = open(path.join(location, str(i) + ".tif"), "wb")
+		fil.write(get_tif(files[-(i*8 + 1)]))
+		fil.close()
+		i += 1
+
+def get_str(index):
+	def set_pixel(x,y,c):
+		if x >= 0 and x < res[0] and y >= 0 and y < res[1]:
+			canvas[y*res[0] + x] = c
+	def draw_outline():
+		def draw_line(x0, y0, x1, y1):
+			x0 = floor((x0-rect[0])*res_scale)
+			y0 = floor((y0-rect[1])*res_scale)
+			x1 = floor((x1-rect[0])*res_scale)
+			y1 = floor((y1-rect[1])*res_scale)
+			set_pixel(x1, y1, 0xf)
+			steep = abs(y1 - y0) > abs(x1 - x0)
+			if steep: x0, y0, x1, y1 = y0, x0, y1, x1
+			if x0 > x1: x0, x1, y0, y1 = x1, x0, y1, y0
+			dx,dy = x1 - x0, abs(y1 - y0)
+			error = dx / 2
+			y = y0
+			if y0 < y1: ystep = 1 
+			else: ystep = -1
+			for x in range(x0, x1):
+				if steep: set_pixel(floor(y), floor(x), 0xf)
+				else: set_pixel(floor(x), floor(y), 0xf)
+				error -= dy
+				if error < 0: y += ystep; error += dx
+		for l in range(len(lines_x)):
+			for j in range(len(lines_x[l])-1):
+				draw_line(
+					lines_x[l][j]  *5,lines_y[l][j]  *5,
+					lines_x[l][j+1]*5,lines_y[l][j+1]*5
+				)
+	def get_radar():
+		cref = Image.open(path.join(cache_dir, str(index) + ".tif"))
+		# Use PIL to crop and resize radar image to fit terminal
+		out = cref.resize(
+			(res[0], res[1]),
+			box=(rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3]),
+			resample=0
+		)
+		w,h = out.size
+		px = out.load()
+		for y in range(0,h):
+			for x in range(0,w):
+				r,g,b,a = px[x,y]
+				if a > 0: set_pixel(x,y,closest([r,g,b]))
+	canvas = [0] * (res[0]*res[1])
+	if args.lines:
+		if args.lines == "top" or args.lines == "above":
+			get_radar()
+			draw_outline()
+		elif args.lines == "bottom" or args.lines == "below":
+			draw_outline()
+			get_radar()
+		else: get_radar()
+	else: get_radar(); draw_outline()
+	if not args.nopins:
+		for p in pins:
+			set_pixel(
+				floor((p[0] - rect[0])*res_scale),
+				floor((p[1] - rect[1])*res_scale),
+				"blink"
+			)
+
+	string = ""
+	for y in range(0, res[1]-2, 2):
+		string += "\n"
+		for x in range(res[0]):
+			pixel1 = canvas[ y   *res[0] + x]
+			pixel2 = canvas[(y+1)*res[0] + x]
+			if (pixel1 == "blink" or pixel2 == "blink"):
+				string += col_bg[0]
+				string += "\033[93;5m"
+				string += chr(0x2588)
+				string += col_bg[0]
+			else:
+				if args.lowres:
+					if pixel2 == 15: string += col_fg[15] # fix holes
+					else: string += col_fg[pixel1]
+					string += chr(0x2588)
+				else:
+					string += col_bg[pixel2]
+					string += col_fg[pixel1]
+					string += chr(0x2580)
+		string += "\033[0;30m"
+	return string
+
+parser = ArgumentParser(description="Show NOAA radar images in the commandline")
+parser.add_argument("--lowres",
+	help="Don't use unicode characters to double resolution",
+	action="store_true")
+parser.add_argument("--size",
+	help="Size (in characters) to render (Default: Fill terminal window)",
+	metavar="WxH")
+parser.add_argument("--lines",
+	help="State outlines. Options: top/above, bottom/below, none (Default: top)")
+parser.add_argument("--nopins",
+	help="Don't draw blinking pins",
+	action="store_true")
+parser.add_argument("--anim",
+	help="Download older frames and show animation (download and display intensive!)",
+	action="store_true")
+parser.add_argument("--frames",
+	help="Number of frames to animate (1 to 6, Default: 3)")
+parser.add_argument("--all",
+	help="Show entire contiguous US",
+	action="store_true")
+parser.add_argument("--update",
+	help="Download new images (Entire US radar is cached, only run this when data is stale!)",
+	action="store_true")
+args = parser.parse_args()
 
 # Find config directory
 from xdg.BaseDirectory import xdg_config_home
@@ -66,9 +224,7 @@ if not path.exists(path.join(conf_dir, "config")):
 
 # Load config file
 try: config.read(path.join(conf_dir, "config"))
-except:
-	print("Could not parse config file, \"" + path.join(conf_dir, "config") + "\", try deleting it.")
-	exit(1)
+except: exit("Could not parse config file, \"" + path.join(conf_dir, "config") + "\", try deleting it.")
 
 # Check if configuration is valid
 rect = []
@@ -78,8 +234,7 @@ if not (
 	rect[1] > 0 and rect[1] + rect[3] < res_img[1]
 ):
 	print("Invalid view rectangle ({0})".format(rect))
-	print("Can't be negative and must be within map size. ({0} x {1})".format(res_img[0], res_img[1]))
-	exit(1)
+	exit("Can't be negative and must be within map size. ({0} x {1})".format(res_img[0], res_img[1]))
 
 pins = []
 for i in split(";", config.get("main", "pins")):
@@ -90,18 +245,10 @@ for i in split(";", config.get("main", "pins")):
 # TODO check if pins are valid
 
 # import terminal colors from config
-# https://stackoverflow.com/questions/54242194/python-find-the-closest-color-to-a-color-from-giving-list-of-colors/54244301#54244301
-def closest(color):
-	# clamp out the mostly useless "colder" colors that are just noise
-	if color[0]*2 + color[1]*1 - color[2]*3 < 128: return 0
-	color = np.array(color)
-	distances = np.sqrt(np.sum((col_actual-color)**2,axis=1))
-	return np.where(distances==np.amin(distances))[0][0]
 # convert "#rrggbb" to "[0xrr, 0xgg, 0xbb]" for numpy
 col_actual = split(",", config.get("main", "termcolors"))
 if len(col_actual) != 16:
-	print("\"termcolors\" configuration option needs 16 values (has " + str(len(col_actual)) + "). Try deleting " + conf_dir)
-	exit(1)
+	exit("\"termcolors\" configuration option needs 16 values (has " + str(len(col_actual)) + "). Try deleting " + conf_dir)
 for c in range(len(col_actual)):
 	col_actual[c] = [
 		int(col_actual[c][0:2], 16),
@@ -110,72 +257,19 @@ for c in range(len(col_actual)):
 	]
 col_actual = np.array(col_actual)
 
-
-
-def is_int(i):
-	try: int(i); return True
-	except ValueError: return False
-
-parser = ArgumentParser(description="Show NOAA radar images in the commandline")
-parser.add_argument("--lowres",
-	help="Don't use unicode characters to double resolution",
-	action="store_true")
-parser.add_argument("--size",
-	help="Size (in characters) to render (Default: Fill terminal window)",
-	metavar="WxH")
-parser.add_argument("--lines",
-	help="State outlines. Options: top/above, bottom/below, none (Default: top)")
-parser.add_argument("--nopins",
-	help="Don't draw blinking pins",
-	action="store_true")
-parser.add_argument("--anim",
-	help="Download older frames and show animation (download and display intensive!)",
-	action="store_true")
-parser.add_argument("--frames",
-	help="Number of frames to animate (1 to 6, Default: 3)")
-parser.add_argument("--all",
-	help="Show entire contiguous US",
-	action="store_true")
-parser.add_argument("--update",
-	help="Download new images (Entire US radar is cached, only run this when data is stale!)",
-	action="store_true")
-
-args = parser.parse_args()
-
-# Terminal control characters for the various colors
-col_fg = [
-	"\033[1;30m","\033[1;31m","\033[1;32m","\033[1;33m",
-	"\033[1;34m","\033[1;35m","\033[1;36m","\033[1;37m",
-	"\033[1;90m","\033[1;91m","\033[1;92m","\033[1;93m",
-	"\033[1;94m","\033[1;95m","\033[1;96m","\033[1;97m",
-]
-col_bg = [
-	"\033[0;40m","\033[0;41m","\033[0;42m","\033[0;43m",
-	"\033[0;44m","\033[0;45m","\033[0;46m","\033[0;47m",
-	"\033[0;100m","\033[0;101m","\033[0;102m","\033[0;103m",
-	"\033[0;104m","\033[0;105m","\033[0;106m","\033[0;107m",
-]
-
-res = [0,0]
-res_scale = 0
-offset = [0,0]
-canvas = []
-
+res = [0,0] # Size of scaled/cropped view
+res_scale = 0 # Scale to terminal
+offset = [0,0] # Crop offset
 res_term = [get_terminal_size().columns, get_terminal_size().lines]
 
 # Check terminal size
 if args.size:
 	s = split('x',args.size)
-	if (is_int(s[0]) and is_int(s[1])):
-		res_term = [int(s[0]), int(s[1]) ]
-	else:
-		print("Error: Invalid size")
-		exit(1)
+	if (is_int(s[0]) and is_int(s[1])): res_term = [int(s[0]), int(s[1]) ]
+	else: exit("Error: Invalid size")
 if args.all: rect = [0, 0, res_img[0], res_img[1]]
 res_term[1] = res_term[1] * 2
-if (res_term[0] < 16 or res_term[1] < 16):
-	print("Terminal window too small (<16x16)")
-	exit(1)
+if (res_term[0] < 16 or res_term[1] < 16): exit("Terminal window too small (<16x16)")
 
 # Hard resolution limit
 res_term[0] = min(res_term[0], 512)
@@ -187,122 +281,6 @@ res = [
 	floor(res_scale*rect[3])
 ]
 
-
-# Scrape TIF images from NOAA using some lazy commands
-def get_map(frames, location):
-	base_url = "https://mrms.ncep.noaa.gov/data/RIDGEII/L2/CONUS/CREF_RAW/"
-	def get_tif(url):
-		tif = run(["wget", "-O", "-", "-o", "/dev/null", base_url + url + ".tif.gz"], check=True, capture_output=True)
-		out = run(["gunzip", "-c"], input=tif.stdout, capture_output=True)
-		return out.stdout
-	for f in glob(location + "/" + "*.tif"):
-		remove(f)
-	# Get latest list of images
-	result = run(["wget", "-O", "-", "-o", "/dev/null", base_url], stdout=PIPE)
-	files = []
-	for line in str(result).split('\\n'):
-		if ".tif.gz" in line:
-			files.append(search('<a href="(.*).tif.gz">', line).group(1))
-	# Download and save to file
-	i = 0
-	for f in range(frames):
-		print("Downloading frame " + str(f))
-		fil = open(path.join(location, str(i) + ".tif"), "wb")
-		fil.write(get_tif(files[-(i*8 + 1)]))
-		fil.close()
-		i += 1
-
-def set_pixel(x,y,c):
-	global canvas
-	if x >= 0 and x < res[0] and y >= 0 and y < res[1]:
-		canvas[y*res[0] + x] = c
-
-def draw_line(x0, y0, x1, y1):
-	x0 = floor((x0-rect[0])*res_scale)
-	y0 = floor((y0-rect[1])*res_scale)
-	x1 = floor((x1-rect[0])*res_scale)
-	y1 = floor((y1-rect[1])*res_scale)
-	set_pixel(x1, y1, 0xf)
-	steep = abs(y1 - y0) > abs(x1 - x0)
-	if steep: x0, y0, x1, y1 = y0, x0, y1, x1
-	if x0 > x1: x0, x1, y0, y1 = x1, x0, y1, y0
-	dx,dy = x1 - x0, abs(y1 - y0)
-	error = dx / 2
-	y = y0
-	if y0 < y1: ystep = 1 
-	else: ystep = -1
-	for x in range(x0, x1):
-		if steep: set_pixel(floor(y), floor(x), 0xf)
-		else: set_pixel(floor(x), floor(y), 0xf)
-		error -= dy
-		if error < 0: y += ystep; error += dx
-
-def draw_outline():
-	for l in range(len(lines_x)):
-		for j in range(len(lines_x[l])-1):
-			draw_line(
-				lines_x[l][j]  *5,lines_y[l][j]  *5,
-				lines_x[l][j+1]*5,lines_y[l][j+1]*5
-			)
-
-def get_radar(index):
-	cref = Image.open(path.join(cache_dir, str(index) + ".tif"))
-	# Use PIL to crop and resize radar image to fit terminal
-	out = cref.resize(
-		(res[0], res[1]),
-		box=(rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3]),
-		resample=0
-	)
-	w,h = out.size
-	px = out.load()
-	for y in range(0,h):
-		for x in range(0,w):
-			r,g,b,a = px[x,y]
-			if a > 0: set_pixel(x,y,closest([r,g,b]))
-
-def get_str(index):
-	global canvas
-	canvas = [0] * (res[0]*res[1])
-	if args.lines:
-		if args.lines == "top" or args.lines == "above":
-			get_radar(index)
-			draw_outline()
-		elif args.lines == "bottom" or args.lines == "below":
-			draw_outline()
-			get_radar(index)
-		else: get_radar(index)
-	else: get_radar(index); draw_outline()
-	if not args.nopins:
-		for p in pins:
-			set_pixel(
-				floor((p[0] - rect[0])*res_scale),
-				floor((p[1] - rect[1])*res_scale),
-				"blink"
-			)
-
-	string = ""
-	for y in range(0, res[1]-2, 2):
-		string += "\n"
-		for x in range(res[0]):
-			pixel1 = canvas[ y   *res[0] + x]
-			pixel2 = canvas[(y+1)*res[0] + x]
-			if (pixel1 == "blink" or pixel2 == "blink"):
-				string += col_bg[0]
-				string += "\033[93;5m"
-				string += chr(0x2588)
-				string += col_bg[0]
-			else:
-				if args.lowres:
-					if pixel2 == 15: string += col_fg[15] # fix holes
-					else: string += col_fg[pixel1]
-					string += chr(0x2588)
-				else:
-					string += col_bg[pixel2]
-					string += col_fg[pixel1]
-					string += chr(0x2580)
-		string += "\033[0;30m"
-	return string
-
 def main():
 	if args.update:
 		frames = 1
@@ -310,8 +288,7 @@ def main():
 		if args.frames: frames = clamp(int(args.frames), 1, 6)
 		get_map(frames, cache_dir)
 	elif len(glob(cache_dir + "/*.tif")) == 0:
-		print("Error: No images, please use --update")
-		exit()
+		exit("Error: No images, please use --update")
 
 	if args.anim:
 		anim = []
@@ -324,8 +301,21 @@ def main():
 			sleep(1.5)
 	else: print(get_str(0))
 
+
+# Map data
+
+
 # https://github.com/amit1rrr/numcompress/
 def decompress(text):
+	def decompress_number(text, index):
+		result,shift = 1,0
+		while True:
+			b = ord(text[index]) - 64
+			index += 1
+			result += b << shift
+			shift += 5
+			if b < 0x1f: break
+		return index, (~result >> 1) if (result & 1) != 0 else (result >> 1)
 	result = []
 	index,last_num = 0,0
 	while index < len(text):
@@ -333,15 +323,6 @@ def decompress(text):
 		last_num += diff
 		result.append(last_num)
 	return result
-def decompress_number(text, index):
-	result,shift = 1,0
-	while True:
-		b = ord(text[index]) - 64
-		index += 1
-		result += b << shift
-		shift += 5
-		if b < 0x1f: break
-	return index, (~result >> 1) if (result & 1) != 0 else (result >> 1)
 
 # Map outlines, encoded to save space
 lx = ("oG`@?CCH?KN@LB@_@k@GD?LCGF@GEEEF?EBD?@HCCEGE@GLCFDeFkEgE}DqDeB?MCIGAOAKAQMBICCIEMYC[CMr@n@OUMAFGMOISEGAGCE@KBGOKOKYUS@e@DM^@^BXNCFF@VNCQGJCHAHE@EGMKMIBH"
@@ -374,4 +355,5 @@ ly = ("_JBNMPADB?LVNBIAGIBO?@A@F?HCAG?IBGFIABAFATFPDBJ??????NCUC@AAC?B?CAAGB@GCD
 lines_x,lines_y = [],[]
 for f in lx.split(","): lines_x.append(decompress(f))
 for f in ly.split(","): lines_y.append(decompress(f))
+
 main()
